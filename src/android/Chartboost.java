6e92ae637799e577fb7b9e0f6ad6b4c8d5068167
Сергей Сагеев <C:\Users\dev\AppData\Roaming\The Bat!>
2019-12-01 16:10:43 +0700

    Initial commit with file structure

---
+++ src/android/Chartboost.java
@@ -0,0 +1,36 @@
+package cordova.plugin.chartboost_ad;
+
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.chartboost.sdk.Chartboost;
+import com.chartboost.sdk.CBLocation;
+import com.chartboost.sdk.ChartboostDelegate;
+
+/**
+ * This class echoes a string called from JavaScript.
+ */
+public class Chartboost extends CordovaPlugin {
+
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        if (action.equals("coolMethod")) {
+            String message = args.getString(0);
+            this.coolMethod(message, callbackContext);
+            return true;
+        }
+        return false;
+    }
+
+    private void coolMethod(String message, CallbackContext callbackContext) {
+        if (message != null && message.length() > 0) {
+            callbackContext.success(message);
+        } else {
+            callbackContext.error("Expected one non-empty string argument.");
+        }
+    }
+}
